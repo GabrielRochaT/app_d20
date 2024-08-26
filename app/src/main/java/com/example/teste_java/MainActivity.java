a9c970777de807da723e0d6e3ee76c95ec5d1eec
GabrielrochaT <joaogabrie487gmail.com>
2024-08-26 17:23:22 -0300

    :tada: init: initializing repo and uploading project

---
+++ app/src/main/java/com/example/teste_java/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.teste_java;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void genNumber(View view){
+        Random rnd = new Random();
+        int number = rnd.nextInt(20);
+        TextView res = findViewById(R.id.result);
+        res.setText(""+number);
+
+
+    }
+}
